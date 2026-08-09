f4594da70992949e3f6695405385442cb0cff48d
leofltt <flttleonardo@gmail.com>
2026-08-09 10:55:00 +0200

    add version 2;

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'audiomultitool-v1';
+const CACHE_NAME = 'audiomultitool-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -15,6 +15,20 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
 });
 
